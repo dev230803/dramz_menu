a9100e061adc82d772bb2645bd39e441741dd9b3
Devansh Singhla <devanshsinghla@gmail.com>
2026-09-09 22:09:40 +0530

    updated model-viewer with ar modes

--- src/components/ModelViewerModal.jsx
+++ src/components/ModelViewerModal.jsx
@@ -19,7 +19,7 @@ const ModelViewerModal = ({ dish, onClose }) => {
           src={dish.model}
           ios-src={dish.modelIos}
           ar
-          ar-modes="webxr scene-viewer quick-look"
+          ar-modes="scene-viewer"
           ar-scale="fixed"
           camera-controls
           auto-rotate
